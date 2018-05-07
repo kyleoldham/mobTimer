d3a082cf8f155a0e3bcd72f84e38b30da427dc55
JourdanLieblich <jslieblich@gmail.com>
2018-05-07 15:26:10 -0700

    completed the remove name feature, next is to link that to the timer

--- src/reducers/reducer_add_name.jsx
+++ src/reducers/reducer_add_name.jsx
@@ -1,6 +1,7 @@
 export default function (state=[], action) {
-  console.log(state);
   switch(action.type){
+    case 'REMOVE_NAME':
+      return state.filter(name => name.name !== action.payload);
 
     case 'ADD_NAME':
       return [ { name: action.payload}, ...state ]; 
